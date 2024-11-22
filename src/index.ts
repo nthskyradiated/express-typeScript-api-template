c1eb6a2a891baf0188a2f109e0b24f8a7215c49e
nthskyradiated <71885584+nthskyradiated@users.noreply.github.com>
2024-11-22 22:27:54 +0400

    Update index.ts for Blue deployment

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ app.use(express.json())
 
 app.get<{}, MessageResponse>('/', (req , res: Response) => {
     res.json({
-        message: "API RootDir"
+        message: "Blue: API RootDir"
     }).end()
 })
 app.use(`${apiDirPath}`, routes)
