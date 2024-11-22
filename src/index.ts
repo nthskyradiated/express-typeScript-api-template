7aa8760869ca060a4492acd9386a84ede58b6e93
nthskyradiated <71885584+nthskyradiated@users.noreply.github.com>
2024-11-22 22:42:20 +0400

    Update index.ts Green deployment

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ app.use(express.json())
 
 app.get<{}, MessageResponse>('/', (req , res: Response) => {
     res.json({
-        message: "Blue: API RootDir"
+        message: "Green: API RootDir"
     }).end()
 })
 app.use(`${apiDirPath}`, routes)
